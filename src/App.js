2d923ca3763f1382ecc9f601ca52ec2a80879ff9
MaryusMartsyalis <martsyalis@gmail.com>
2018-07-10 16:02:32 -0700

    set up gallery

--- src/App.js
+++ src/App.js
@@ -1,10 +1,25 @@
-import React, { PureComponent } from 'react';
+import React, { PureComponent, Fragment } from 'react';
+import giantJpeg from './assets/giant.jpg';
+import giantPng from './assets/giant.png';
+import giantSvg from './assets/giant.svg';
+import largeJpeg from './assets/large.jpg';
+import largePng from './assets/large.png';
+import smallJpg from './assets/small.jpg';
+import smallPng from './assets/small.png';
+import icon from './assets/icon.ico';
+import Gallery from './Gallery';
 import './App.css';
 
+const imageArray = [giantJpeg, giantPng, giantSvg, largeJpeg, largePng, smallJpg, smallPng, icon];
+
 export default class App extends PureComponent {
+
   render() {
     return (
-      <div className="greeting">Hello world!</div>
+      <Fragment>
+        <div className="greeting">Hello world!</div>
+        <Gallery imageArray={imageArray} />
+      </Fragment>
     );
   }
 }
---
+++ src/Gallery.js
@@ -0,0 +1,26 @@
+import React, { PureComponent } from 'react';
+import PropTypes from 'prop-types';
+import './Gallery.css';
+
+export default class Gallery extends PureComponent {
+  static propTypes = {
+    imageArray: PropTypes.array
+  };
+
+  render() {
+    const { imageArray } = this.props;
+    return (
+      <div className="gallery">
+        {imageArray && imageArray.map((image, i) => {
+          return (
+            <div key={i} >
+              <h2>index: {i}</h2>
+              <img src={image} alt="test" className="gallery-image" />
+            </div>
+          );
+        })}
+      </div>
+    );
+  }
+}
+
